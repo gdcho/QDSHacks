39e96fc54faac8b3bfd9bbf52bf3e5f3a4f1d677
gdcho <gcho13@my.bcit.ca>
2024-03-09 17:54:23 -0800

    changes to styling

--- src/components/shared/buttons/google-login.tsx
+++ src/components/shared/buttons/google-login.tsx
@@ -1,27 +1,42 @@
 import { signIn } from "next-auth/react";
 import { Button } from "@mui/material";
 import { Google } from "@mui/icons-material";
+import Image from "next/image";
 
 export default function GoogleLoginButton() {
-    return (
+  return (
+    <>
+      <div className="flex justify-center pt-10">
+        <div className="relative h-40 w-80">
+          <Image
+            fill
+            sizes="(max-width: 768px) 50vw, (max-width: 1200px) 30vw, 30vw"
+            className="object-contain"
+            src="/image/site_logo.png"
+            alt="Site logo"
+            priority
+          />
+        </div>
+      </div>
       <Button
-        variant="contained" 
-        color="primary" 
+        variant="contained"
+        color="primary"
         onClick={() => signIn("google", { callbackUrl: "/" })}
-        className="!text-white !bg-blue-500 hover:!bg-blue-700" 
+        className="!text-white !bg-blue-500 hover:!bg-blue-700"
         sx={{
           py: 2,
-          boxShadow: 'none', 
-          '&:hover': {
-            boxShadow: 'none',
+          boxShadow: "none",
+          "&:hover": {
+            boxShadow: "none",
           },
           gap: 1,
-          mt: 10,
+          mt: 6,
           borderRadius: 10,
         }}
       >
-        <Google /> 
-        Google Log-in
+        <Google />
+        Google Sign-in
       </Button>
-    );
-};
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
-import StressStatsDisplay from './stressStatsDisplay';
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import StressStatsDisplay from "./stressStatsDisplay";
 
 export default function Home() {
   const router = useRouter();
@@ -13,28 +13,33 @@ export default function Home() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('/api/mongo');
+        const response = await fetch("/api/mongo");
         const jsonData = await response.json();
 
-        setUserData(jsonData);        
+        setUserData(jsonData);
 
-        if (session && !jsonData.some((user: { user_id: string; }) => user.user_id === session.user?.id)) {
+        if (
+          session &&
+          !jsonData.some(
+            (user: { user_id: string }) => user.user_id === session.user?.id
+          )
+        ) {
           try {
-            console.log('Session:', session);
-            await axios.post('/api/addUser', {
+            console.log("Session:", session);
+            await axios.post("/api/addUser", {
               user_id: session.user?.id,
               name: session.user?.name,
-              session_email: session.user?.email
+              session_email: session.user?.email,
             });
-            console.log('User data inserted successfully');
+            console.log("User data inserted successfully");
           } catch (error) {
-            console.error('Error inserting user data:', error);
+            console.error("Error inserting user data:", error);
           }
 
-          router.push('/profile');
+          router.push("/profile");
         }
       } catch (error) {
-        console.error('Error:', error);
+        console.error("Error:", error);
       }
     };
     fetchData();
@@ -43,51 +48,50 @@ export default function Home() {
   return (
     <div>
       <br />
-      {!session ? (
-        <p className="px-4 md:px-0 text-center text-gray-600 dark:text-gray-400 max-w-2xl mx-auto mb-10">
-          Starter hack template for hackathons, created by David. This is the
-          homepage. Start by editing{" "}
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-      ) : (
-        <div className="px-4 md:px-0 text-center text-gray-600 dark:text-gray-400 max-w-2xl mx-auto mb-10">
-          Welcome to <strong>StressBuddies</strong>!
-          <div className="flex justify-center mt-4">
-            
-            <div className="relative h-40 w-80"> 
-              <Image
-                fill
-                sizes="(max-width: 768px) 50vw, (max-width: 1200px) 30vw, 30vw"
-                className="object-contain" 
-                src="/image/site_logo.png"
-                alt="Site logo"
-                priority
-              />
-            </div>
-            </div>
-            <div className="flex flex-col items-center mt-4">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-12 rounded-lg text-2xl cursor-pointer"
-              onClick={() => router.push('/connect')}
-            >
-              Find your buddy!
-            </button>
+      <div className="px-4 md:px-0 text-center text-gray-600 dark:text-gray-400 max-w-2xl mx-auto mb-10">
+        Welcome to <strong>StressBuddies</strong>
+        <div className="flex justify-center mt-4">
+          <div className="relative h-40 w-80">
+            <Image
+              fill
+              sizes="(max-width: 768px) 50vw, (max-width: 1200px) 30vw, 30vw"
+              className="object-contain"
+              src="/image/site_logo.png"
+              alt="Site logo"
+              priority
+            />
           </div>
-            <br></br>
-            <div className="bg-gray-200 text-center p-4 mx-4 md:mx-0 rounded-lg shadow-md">
-            <p className="text-gray-600 dark:text-gray-800">
-              At <strong>StressBuddies</strong> we aim to tackle the problem at the root cause.
-              The main cause of stress in students comes from overloaded workload and hard assignments.
-              That’s why your stress buddy can help you with your work, and you can help them with yours!
-              We aim to reduce your stress, and enhance your well-being by tackling the root problem.
-            </p>
-          </div>
-          <br></br>
-          {/* <div className="bg-green-200 text-center p-4 mx-4 md:mx-0 rounded-lg shadow-md my-8">
+        </div>
+        <div className="flex flex-col items-center mt-4">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-12 rounded-full text-2xl cursor-pointer"
+            onClick={() => {
+              if (session) {
+                router.push("/connect");
+              } else {
+                router.push("/profile");
+              }
+            }}
+          >
+            Find your buddy
+          </button>
+        </div>
+        <br></br>
+        <div className="bg-gray-200 text-center p-4 mx-4 md:mx-0 rounded-lg shadow-md">
+          <p className="text-gray-600 dark:text-gray-800">
+            At <strong>StressBuddies</strong> we aim to tackle the problem at
+            the root cause. The main cause of stress in students comes from
+            overloaded workload and hard assignments. That’s why your stress
+            buddy can help you with your work, and you can help them with yours!
+            We aim to reduce your stress, and enhance your well-being by
+            tackling the root problem.
+          </p>
+        </div>
+        <br></br>
+        {/* <div className="bg-green-200 text-center p-4 mx-4 md:mx-0 rounded-lg shadow-md my-8">
             <StressStatsDisplay />
           </div> */}
-        </div>   
-      )}
+      </div>
     </div>
   );
 }
